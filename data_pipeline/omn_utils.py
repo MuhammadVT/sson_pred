6fca0c1b75fa24c8aa767d4cc06cca4a476c19d4
bharatreddy <bharatr@vt.edu>
2018-12-20 16:08:02 -0500

    renamed file and omn utils file

--- data_pipeline/omn_utils.py
+++ data_pipeline/omn_utils.py
@@ -8,9 +8,10 @@ class OmnData(object):
     """
     Utils to work with omni data
     """
-    def __init__(self, start_date, end_date, omn_dbdir, \
-                omn_db_name, omn_table_name, omn_train, imf_normalize, model_path, db_time_resolution=5,\
-                omn_train_params = [ "By", "Bz", "Vx", "Np" ]):
+    def __init__(self, start_date, end_date, omn_dbdir, omn_db_name,\
+                 omn_table_name, omn_train, omn_norm_param_file,\
+                imf_normalize=True, db_time_resolution=1,\
+                omn_train_params = [ "By", "Bz", "Bx" ]):
         """
         setup some vars
         """
@@ -23,7 +24,7 @@ class OmnData(object):
         self.omn_train_params = omn_train_params
         self.omn_train = omn_train
         self.imf_normalize = imf_normalize
-        self.model_path = model_path
+        self.omn_norm_param_file = omn_norm_param_file
         self.omnDF = self._load_omn_data()
 
     def _load_omn_data(self):
@@ -33,8 +34,7 @@ class OmnData(object):
         conn = sqlite3.connect(self.omn_dbdir + self.omn_db_name,
                        detect_types = sqlite3.PARSE_DECLTYPES)
         # load data to a dataframe
-        command = "SELECT * FROM {tb} " +\
-                  "WHERE datetime BETWEEN '{stm}' and '{etm}'"
+        command = "SELECT * FROM {tb} "
         command = command.format(tb=self.omn_table_name,\
                                  stm=self.start_date, etm=self.end_date)
         omnDF = pandas.read_sql(command, conn)
@@ -47,6 +47,7 @@ class OmnData(object):
         while curr_time <= self.end_date:
             new_omn_index_arr.append( curr_time )
             curr_time += datetime.timedelta(minutes=self.db_time_resolution)
+        omnDF = omnDF[ omn_train_params ]
         omnDF = omnDF.replace(numpy.inf, numpy.nan)
         omnDF = omnDF.set_index("datetime").reindex(new_omn_index_arr).reset_index()
         # Replace nan's with preceding value (forward filling)
@@ -60,9 +61,9 @@ class OmnData(object):
                 #Storing the current mean and std which will be used to normalize the test data (in get_prediction file)
 
                 mean_std_values = (omnDF[self.omn_train_params].mean(), omnDF[self.omn_train_params].std())
-                print ("mean and std values...")
-                print (mean_std_values)    
-                numpy.save(self.model_path+'/mean_std', mean_std_values)
+                print("mean and std values...")
+                print(mean_std_values)    
+                numpy.save(self.omn_norm_param_file, mean_std_values)
 
                 #This operation does the column wise normalization only on the selected columns             
                 omnDF[self.omn_train_params] = omnDF[self.omn_train_params].apply(lambda x: (x - x.mean()) / x.std())
@@ -70,7 +71,7 @@ class OmnData(object):
                 
                 #this part will be called when get_prediction file is run for getting the predicted values
                 print ("Using the mean_std.npy file for IMF normalization ...")
-                mean_std_values = numpy.load(self.model_path+'/mean_std.npy')
+                mean_std_values = numpy.load(self.omn_norm_param_file)
                 col_means, col_stds = mean_std_values
                 print ("mean is:", col_means)
                 print ("std is:", col_stds)
@@ -78,6 +79,5 @@ class OmnData(object):
                 i = 0
                 for imf in self.omn_train_params:
                     omnDF[imf] = (omnDF[imf] - col_means[i]) / col_stds[i]   
-                    i += 1
-                      
-return omnDF
+                    i += 1  
+        return omnDF
