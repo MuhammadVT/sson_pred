6453288066c9ae41ee5b2f99178850c5caf77155
Maimaitirebike Maimaiti <rafiq@calogin1.ca.arc.internal>
2018-12-28 11:24:45 -0500

    add weights to binary_crossentropy to mitigate class imbalance

--- models/fcnn_main.py
+++ models/fcnn_main.py
@@ -17,8 +17,8 @@ import sys
 sys.path.append("../data_pipeline")
 #import batch_utils
 
-classes_are_exclusive = False    # Corresponds to sigmoid activation layer
-#classes_are_exclusive = True    # Corresponds to softmax activation layer
+classes_are_mutually_exclusive = False    # Corresponds to sigmoid activation layer
+#classes_are_mutually_exclusive = True    # Corresponds to softmax activation layer
 
 # Load the data
 print("loading the data...")
@@ -36,7 +36,7 @@ output_file = "../data/output.nBins_6.binTimeRes_10.onsetFillTimeRes_1.shuffleDa
 
 X = np.load(input_file)
 y = np.load(output_file)
-if classes_are_exclusive:
+if classes_are_mutually_exclusive:
     #y = y[:, 0:2]    # two classes
     #y = y[:, 0]    # one class
     enc = OneHotEncoder()
@@ -45,23 +45,50 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.20, random_state=10)
 
 # Build a FCNN model
-loss=keras.losses.categorical_crossentropy
 optimizer=keras.optimizers.Adam(lr=0.0001)
-batch_size = 32
-n_epochs = 100
+batch_size = 64
+n_epochs = 200
 n_classes = y_train.shape[1] 
+metrics = ["accuracy"]
 input_shape = x_train.shape[1:]
-out_dir="./trained_models/FCNN/" + dt.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Set class weight
+def binary_crossentropy_weigted(y_true, y_pred):
+    import keras.backend as K
+    class_weights = K.variable(np.array([5]))
+    #class_weights = np.array([5, 1])
+    y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
+    #loss = K.mean(class_weights*(-y_true * K.log(y_pred) - (1.0 - y_true) * K.log(1.0 - y_pred)),axis=-1)
+    loss = K.mean((-y_true * K.log(y_pred) * class_weights - (1.0 - y_true) * K.log(1.0 - y_pred)),axis=-1)
+    return loss
+
+#from sklearn import utils
+#class_weights = utils.class_weight.compute_class_weight("balanced", np.unique(y_train) 
+#class_weights = {i:(1./y_train[:,i].mean()) for i in range(n_classes)}
+#class_weights = {0:10, 1:1, 2:1, 3:1, 4:1, 5:1}
+class_weights = None
+
 # create out_dir
+out_dir="./trained_models/FCNN/" + dt.datetime.now().strftime("%Y%m%d_%H%M%S")
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
+
+# Define the loss
+if classes_are_mutually_exclusive:
+    loss=keras.losses.categorical_crossentropy
+else:
+    #loss=keras.losses.binary_crossentropy
+    loss = binary_crossentropy_weigted
+
+
 fcnn = FCNN(input_shape, batch_size=batch_size, n_epochs=n_epochs,
             n_classes=n_classes, loss=loss, optimizer=optimizer,
-            metrics=["accuracy"], out_dir=out_dir)
+            metrics=metrics, out_dir=out_dir)
 
 # Train the model
 print("Training the model...")
-fit_history = fcnn.train_model(x_train, y_train, x_val, y_val, y_test)
+fit_history = fcnn.train_model(x_train, y_train, x_val, y_val, y_test,
+                               class_weight=class_weights)
 
 # Plot the training 
 fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True)
@@ -88,10 +115,10 @@ print("Evaluating the model...")
 #fname = "weights.epoch_{epoch}.val_loss_{val_loss}.val_acc_{val_acc}.hdf5"
 test_epoch = n_epochs
 model_name = glob.glob(os.path.join(out_dir, "weights.epoch_" + str(test_epoch) + "*hdf5"))[0]
-test_model = keras.models.load_model(model_name) 
+test_model = keras.models.load_model(model_name, custom_objects={'binary_crossentropy_weigted': binary_crossentropy_weigted}) 
 y_pred = test_model.predict(x_test, batch_size=32)
 
-if classes_are_exclusive:
+if classes_are_mutually_exclusive:
     # The final activation layer uses softmax
     y_pred = np.argmax(y_pred , axis=1)
     y_true = np.argmax(y_test , axis=1)
