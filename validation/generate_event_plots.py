5e897aed1377907bdf1a95892039c479783b3898
Bharat Simha Reddy Kunduri <bharatr@nrlogin1.cluster>
2019-01-21 14:17:09 -0500

    generate event plots on arc

--- validation/generate_event_plots.py
+++ validation/generate_event_plots.py
@@ -5,7 +5,7 @@ import datetime
 import matplotlib
 matplotlib.use('Agg')
 
-omnDBDir = "/home/bharat/Documents/data/ss_onset_dataset/data/sqlite3/"
+omnDBDir = "../data/sqlite3/"
 omnDbName = "omni_sw_imf.sqlite"
 omnTabName = "imf_sw"
 aulDbName = "au_al_ae.sqlite"
@@ -14,10 +14,11 @@ smlDbName = "smu_sml_sme.sqlite"
 smlTabName = "smusmlsme"
 #predFname = "/home/bharat/Documents/data/ss_onset_dataset/data/all_data.nBins_2.binTimeRes_30.onsetFillTimeRes_1.onsetDelTCutoff_2.omnHistory_120.omnDBRes_1.shuffleData_True.csv"
 predFname = "../data/test_data.pred.test"
+figDir = "../data/plots/sml_onset_plots/"
 
 nBins=1
 binTimeRes=60
 
 psObj = pred_act_cmpr.PredSumry(predFname, nBins=nBins, binTimeRes=binTimeRes)
 psObj.create_pred_plots(omnDBDir, omnDbName, omnTabName, aulDbName,\
-                aulTabName, smlDbName, smlTabName)
+                aulTabName, smlDbName, smlTabName, figDir=figDir)
