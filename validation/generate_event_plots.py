a2796527c391ebb8d3833cc515776cb8d0e37634
bharatreddy <bharatr@vt.edu>
2019-01-21 13:36:23 -0500

    plot prob in event plots

--- validation/generate_event_plots.py
+++ validation/generate_event_plots.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings("ignore")
 import pred_act_cmpr
 import datetime
 import matplotlib
@@ -10,8 +12,12 @@ aulDbName = "au_al_ae.sqlite"
 aulTabName = "aualae"
 smlDbName = "smu_sml_sme.sqlite"
 smlTabName = "smusmlsme"
-predFname = "/home/bharat/Documents/data/ss_onset_dataset/data/all_data.nBins_2.binTimeRes_30.onsetFillTimeRes_1.onsetDelTCutoff_2.omnHistory_120.omnDBRes_1.shuffleData_True.csv"
+#predFname = "/home/bharat/Documents/data/ss_onset_dataset/data/all_data.nBins_2.binTimeRes_30.onsetFillTimeRes_1.onsetDelTCutoff_2.omnHistory_120.omnDBRes_1.shuffleData_True.csv"
+predFname = "../data/test_data.pred.test"
 
-psObj = pred_act_cmpr.PredSumry(predFname)
+nBins=1
+binTimeRes=60
+
+psObj = pred_act_cmpr.PredSumry(predFname, nBins=nBins, binTimeRes=binTimeRes)
 psObj.create_pred_plots(omnDBDir, omnDbName, omnTabName, aulDbName,\
-                aulTabName, smlDbName, smlTabName, nBins=2)
+                aulTabName, smlDbName, smlTabName)
