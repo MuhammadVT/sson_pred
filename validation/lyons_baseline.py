145fee07965da00a5d83e43cb1fe60d40952a5a3
MuhammadVT <mamatripik@gmail.com>
2019-04-03 12:55:48 -0400

    add loat_omn_data function to prevent from filling, did iterpolation to NaN data

--- validation/lyons_baseline.py
+++ validation/lyons_baseline.py
@@ -13,6 +13,35 @@ if module_path not in sys.path:
     sys.path.append(module_path)
 import omn_utils
 
+def load_omn_data(omnStartDate, omnEndDate, omnDBDir,
+                  omnDbName, omnTabName,
+                  db_time_resolution=1,
+                  omn_train_params = ["By", "Bz", "Bx", "Vx", "Np"]):
+    """
+    Load omni data
+    """
+    conn = sqlite3.connect(omnDBDir + omnDbName,
+                   detect_types = sqlite3.PARSE_DECLTYPES)
+    # load data to a dataframe
+    command = "SELECT * FROM {tb} WHERE datetime BETWEEN '{stm}' AND '{etm}'"
+    command = command.format(tb=omnTabName,\
+                             stm=omnStartDate, etm=omnEndDate)
+    omnDF = pandas.read_sql(command, conn)
+    omnDF = omnDF[ omn_train_params + [ "datetime" ] ]
+    omnDF = omnDF.replace(numpy.inf, numpy.nan)
+    omnDF = omnDF.set_index("datetime")
+    # Add omnStartDate to omnDF in case if it is missing
+    if omnStartDate not in omnDF.index:
+        omnDF.loc[omnStartDate] = numpy.nan
+    # Add omnEndDate to omnDF in case if it is missing
+    if omnEndDate not in omnDF.index:
+        omnDF.loc[omnEndDate] = numpy.nan
+    omnDF.sort_index(inplace=True)
+    # Resample the data
+    omnDF = omnDF.resample(str(db_time_resolution) + "Min").asfreq().reset_index()
+
+    return omnDF
+
 # set some variables
 # omn db
 omnDBDir = "../data/sqlite3/"
@@ -23,7 +52,7 @@ omnTimeDelay = 10 #minutes
 # onset data
 smlFname="../data/filtered-20190103-22-53-substorms.csv"
 # some plotting vars
-predDateRange = [datetime.datetime(1996,1,1), datetime.datetime(1998,1,1)]#None
+predDateRange = [datetime.datetime(1996,1,1), datetime.datetime(1997,1,1)]#None
 
 # load onset data
 ssOnsetDF = pandas.read_csv(smlFname,\
@@ -40,15 +69,13 @@ else:
     omnStartDate = ssOnsetDF["date"].min() - datetime.timedelta(minutes=60)
     omnEndDate = ssOnsetDF["date"].max()
 # create the obj and load data
-omnObj = omn_utils.OmnData(omnStartDate, omnEndDate, omnDBDir,\
-                   omnDbName, omnTabName,\
-                   True, None,\
-                    imf_normalize=False,\
-                    db_time_resolution=1,\
-                    omn_train_params = omnParams)
+omnDF = load_omn_data(omnStartDate, omnEndDate, omnDBDir,
+                      omnDbName, omnTabName,
+                      db_time_resolution=1,
+                      omn_train_params = omnParams)
 # add a 10 minute time delay to the omni dataset
-omnObj.omnDF["datetime"] = omnObj.omnDF["datetime"] + datetime.timedelta(minutes=omnTimeDelay)
-omnObj.omnDF.set_index(omnObj.omnDF["datetime"], inplace=True)
+omnDF["datetime"] = omnDF["datetime"] + datetime.timedelta(minutes=omnTimeDelay)
+omnDF.set_index("datetime", inplace=True)
 
 # loop through and make the predictions
 # using the conditions
@@ -59,12 +86,33 @@ predDict["triggerTime"] = []
 nUnknown = 0
 nFalse = 0
 nTrue = 0
+timeBeforeOnset = 29
+timeAfterOnset = 15
+nOmnPnts = timeBeforeOnset + timeAfterOnset + 1
 # if predDateRange is None:
 for _index, _row in ssOnsetDF.iterrows():
-    # get the corresponding omni data
-    _sTime = _row["date"] - datetime.timedelta(minutes=30)
+    # get omni data for need for current Onset
+    _sTm = _row["date"] - datetime.timedelta(minutes=timeBeforeOnset)
+    _eTm = _row["date"] + datetime.timedelta(minutes=timeAfterOnset)
+    omnDF_tmp = omnDF.loc[ _sTm : _eTm]
+
+    # Interpolate the omni data if # missing data points are small
+    # If not discard this time interval
+    if omnDF_tmp.dropna().shape[0] >= nOmnPnts - 5:
+        if omnDF_tmp.dropna().shape[0] < nOmnPnts:
+            omnDF_tmp.interpolate(method="linear", limit_direction="both", inplace=True)
+    else:
+        print("insufficient data", omnDF_tmp.shape) 
+        predDict["date"].append( _row["date"] )
+        predDict["prediction"].append( "U" )
+        predDict["triggerTime"].append( 1000. )
+        nUnknown += 1
+        continue
+
+    # get the corresponding omni data need for Rule 1
+    _sTime = _row["date"] - datetime.timedelta(minutes=timeBeforeOnset)
     _eTime = _row["date"]
-    _currOmn = omnObj.omnDF[ _sTime : _eTime ].dropna()
+    _currOmn = omnDF_tmp.loc[ _sTime : _eTime]
     _criteria1 = False
     _criteria2 = False
     _criteria3 = False
@@ -75,12 +123,8 @@ for _index, _row in ssOnsetDF.iterrows():
     #           values of Bz (sometimes data is missing)
     # there should be 30 values
     if _currOmn.shape[0] < 30:
-        print("insufficient data", _currOmn.shape) 
-        predDict["date"].append( _row["date"] )
-        predDict["prediction"].append( "U" )
-        predDict["triggerTime"].append( 1000. )
-        nUnknown += 1
-        continue
+        import pdb
+        pdb.set_trace()
     else:
         # count number of negative Bz
         nBz = _currOmn[_currOmn["Bz"] < 0.].shape[0]
@@ -101,8 +145,8 @@ for _index, _row in ssOnsetDF.iterrows():
     #            we'll take times +/- 15 minutes near the onset
     #           and see if we can find any rapid Bz northward turning
     _stOnset = _row["date"] - datetime.timedelta(minutes=15)
-    _etOnset = _row["date"] + datetime.timedelta(minutes=15)
-    _omniNearOnset = omnObj.omnDF[ _stOnset : _etOnset ].dropna()
+    _etOnset = _row["date"] + datetime.timedelta(minutes=timeAfterOnset)
+    _omniNearOnset = omnDF[ _stOnset : _etOnset ].dropna()
     _omniNearOnset["delTOnset"] = (_omniNearOnset["datetime"] - _row["date"]).astype('timedelta64[m]')
     _omniNearOnset["delTAbs"] = numpy.abs( _omniNearOnset["delTOnset"] )    
     _omniNearOnset["diffBz"] = _omniNearOnset["Bz"].diff()
@@ -132,8 +176,8 @@ for _index, _row in ssOnsetDF.iterrows():
     #           b) Bz(t0 : t0 +3) >= Bz(t0) + 0.15
     #           c) Bz(t0+3:t0+10) >= Bz(t0) + 0.45
     # get the data for the next _lyonsTriggerTime to _lyonsTriggerTime + 10
-    _triggerDFClstOnset = omnObj.omnDF[ _lyonsTriggerTimeClstOnset + datetime.timedelta(minutes=1) : (_lyonsTriggerTimeClstOnset + datetime.timedelta(minutes=10)) ].dropna()
-    _triggerDFHighestChng = omnObj.omnDF[ _lyonsTriggerTimeHighestChng + datetime.timedelta(minutes=1) : (_lyonsTriggerTimeHighestChng + datetime.timedelta(minutes=10)) ].dropna()
+    _triggerDFClstOnset = omnDF[ _lyonsTriggerTimeClstOnset + datetime.timedelta(minutes=1) : (_lyonsTriggerTimeClstOnset + datetime.timedelta(minutes=10)) ].dropna()
+    _triggerDFHighestChng = omnDF[ _lyonsTriggerTimeHighestChng + datetime.timedelta(minutes=1) : (_lyonsTriggerTimeHighestChng + datetime.timedelta(minutes=10)) ].dropna()
     _slopeClstOnset, _interceptClstOnset, _r_valueClstOnset, _p_valueClstOnset, _std_errClstOnset = stats.linregress( numpy.arange(_triggerDFClstOnset["Bz"].shape[0]), _triggerDFClstOnset["Bz"])
     _slopeClstHighestChng, _interceptHighestChng, _r_valueHighestChng, _p_valueHighestChng, _std_errHighestChng = stats.linregress( numpy.arange(_triggerDFHighestChng["Bz"].shape[0]), _triggerDFHighestChng["Bz"])
 
@@ -154,8 +198,6 @@ for _index, _row in ssOnsetDF.iterrows():
             _bzOnsetVal = _selDF[ _selDF["datetime"] == _lyonsTriggerTimeHighestChng]["Bz"].tolist()[0]
             _trigTime = _lyonsTriggerTimeHighestChng
         _selDF["delOnsetBz"] = _selDF["Bz"] - _bzOnsetVal
-        import pdb
-        pdb.set_trace()
         _sel3MinBz = _selDF[ _selDF.index.min() + datetime.timedelta(minutes=2) : _selDF.index.min() + datetime.timedelta(minutes=3) ]
         _sel3to10MinBz = _selDF[ _selDF.index.min() + datetime.timedelta(minutes=4) : _selDF.index.min() + datetime.timedelta(minutes=10) ]
         if ( (_sel3MinBz["Bz"].min() >= 0.15) & (_sel3to10MinBz["Bz"].min() >= 0.45) ):
@@ -175,4 +217,7 @@ print("nFalse-->", nFalse)
 print("nUnknown-->", nUnknown)
 print("%Pred-->",nTrue*1./(nTrue+nFalse))
 print("%failed-->",nFalse*1./(nTrue+nFalse))
+
+
+
 
